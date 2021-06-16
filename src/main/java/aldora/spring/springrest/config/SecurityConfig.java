a8f4803ffe3a70e3e5f3f51c7531e20b7220c0f0
ShakalakaB <yunjie989@gmail.com>
2021-06-17 01:17:55 +0800

    Restrict to IP address

--- src/main/java/aldora/spring/springrest/config/SecurityConfig.java
+++ src/main/java/aldora/spring/springrest/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package aldora.spring.springrest.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,10 +9,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private Environment environment;
+
+    public SecurityConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/api/**").permitAll();
+//        http.authorizeRequests().antMatchers("/api/**").permitAll();
+//        http.authorizeRequests().antMatchers("/api/**").permitAll();
+        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
         http.headers().frameOptions().disable();
     }
 }
