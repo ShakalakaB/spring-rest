09862cf1703e5f0347f143581d67d28f001e12d6
ShakalakaB <yunjie989@gmail.com>
2021-07-18 23:37:34 +0800

    Edit config for actuator

--- src/main/java/aldora/spring/springrest/config/SecurityConfig.java
+++ src/main/java/aldora/spring/springrest/config/SecurityConfig.java
@@ -26,7 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/api/**").permitAll()
+        http.authorizeRequests()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/actuator/**").permitAll()
                 .and()
                 .addFilter(getAuthenticationFilter());
 //        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
