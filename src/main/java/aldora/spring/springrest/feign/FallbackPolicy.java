ad65f25a4f095364e50e139679ddcbefc7f9cf01
ShakalakaB <yunjie989@gmail.com>
2021-09-11 22:13:14 +0800

    Add log in fallback

--- src/main/java/aldora/spring/springrest/feign/FallbackPolicy.java
+++ src/main/java/aldora/spring/springrest/feign/FallbackPolicy.java
@@ -1,10 +1,12 @@
 package aldora.spring.springrest.feign;
 
 import aldora.spring.springrest.api.v1.model.CustomerDTO;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 public class FallbackPolicy implements AccountFeignService {
     private Exception exception;
 
@@ -24,6 +26,7 @@ public class FallbackPolicy implements AccountFeignService {
 
     @Override
     public List<CustomerDTO> exceptionWithCircuitBreaker() {
+        log.error("exception happend");
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setFirstName("kk");
         return Collections.singletonList(customerDTO);
